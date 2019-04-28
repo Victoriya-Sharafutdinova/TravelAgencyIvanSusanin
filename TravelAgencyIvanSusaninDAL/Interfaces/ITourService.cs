01d0da827383371e53b3e4eabcb5f750d2945625
Danil <anikyev95@mail.ru>
2019-04-28 21:42:41 +0400

    Интерфейсы администратор

---
+++ TravelAgencyIvanSusaninDAL/Interfaces/ITourService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using TravelAgencyIvanSusaninDAL.BindingModel;
+using TravelAgencyIvanSusaninDAL.ViewModel;
+
+namespace TravelAgencyIvanSusaninDAL.Interfaces
+{
+    public interface ITourService
+    {
+        List<TourViewModel> GetList();
+
+        TourViewModel GetElement(int id);
+
+        void AddElement(TourBindingModel model);
+
+        void UpdElement(TourBindingModel model);
+
+        void DelElement(int id);
+    }
+}
