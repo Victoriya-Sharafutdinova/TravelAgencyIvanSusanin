6b24d76db6cdd2af6a7f480d33c40513c7244780
Данил Аникьев <anikyev95@mail.ru>
2019-04-28 13:08:31 +0400

    6 этап: binding и view models

---
+++ TravelAgencyIvanSusaninDAL/BindingModel/ReservationRequestBindingModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.Serialization;
+
+namespace TravelAgencyIvanSusaninDAL.BindingModel
+{
+    [DataContract]
+    public class ReservationRequestBindingModel
+    {
+        public int Id { get; set; }
+
+        [DataMember]
+        public int ReservationId { get; set; }
+
+        [DataMember]
+        public int RequestId { get; set; }
+
+        [DataMember]
+        public int NumberReservation { get; set; }
+    }
+}
---
+++ TravelAgencyIvanSusaninDAL/BindingModel/TourBindingModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.Serialization;
+
+namespace TravelAgencyIvanSusaninDAL.BindingModel
+{
+    [DataContract]
+    public class TourBindingModel
+    {
+        public int Id { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string Description { get; set; }
+
+        [DataMember]
+        public int Cost { get; set; }
+    }
+}
