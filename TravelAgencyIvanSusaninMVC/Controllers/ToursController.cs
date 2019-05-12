ccc01e9e02d0cfadf62a5ba9c62a609e3e97c48f
Вика <kusyshar@gmail.com>
2019-05-12 22:15:29 +0400

    Web клиент

---
+++ TravelAgencyIvanSusaninMVC/Controllers/ToursController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TravelAgencyIvanSusaninMVC.Models;
+using TravelAgencyIvanSusaninModel;
+
+namespace TravelAgencyIvanSusaninMVC.Controllers
+{
+    public class ToursController : Controller
+    {
+        private Context db = new Context();
+
+        // GET: Tours
+        public ActionResult Index()
+        {
+            return View(db.Tours.ToList());
+        }
+
+        // GET: Tours/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tour tour = db.Tours.Find(id);
+            if (tour == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tour);
+        }
+
+        // GET: Tours/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tours/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Description,Cost")] Tour tour)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tours.Add(tour);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tour);
+        }
+
+        // GET: Tours/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tour tour = db.Tours.Find(id);
+            if (tour == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tour);
+        }
+
+        // POST: Tours/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Description,Cost")] Tour tour)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tour).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tour);
+        }
+
+        // GET: Tours/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tour tour = db.Tours.Find(id);
+            if (tour == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tour);
+        }
+
+        // POST: Tours/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Tour tour = db.Tours.Find(id);
+            db.Tours.Remove(tour);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ TravelAgencyIvanSusaninMVC/Controllers/TravelsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TravelAgencyIvanSusaninMVC.Models;
+using TravelAgencyIvanSusaninModel;
+
+namespace TravelAgencyIvanSusaninMVC.Controllers
+{
+    public class TravelsController : Controller
+    {
+        private Context db = new Context();
+
+        // GET: Travels
+        public ActionResult Index()
+        {
+            var travels = db.Travels.Include(t => t.Client);
+            return View(travels.ToList());
+        }
+
+        // GET: Travels/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Travel travel = db.Travels.Find(id);
+            if (travel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(travel);
+        }
+
+        // GET: Travels/Create
+        public ActionResult Create()
+        {
+            ViewBag.ClientId = new SelectList(db.Clients, "Id", "FIO");
+            return View();
+        }
+
+        // POST: Travels/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,ClientId,DateCreate,TotalCost")] Travel travel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Travels.Add(travel);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ClientId = new SelectList(db.Clients, "Id", "FIO", travel.ClientId);
+            return View(travel);
+        }
+
+        // GET: Travels/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Travel travel = db.Travels.Find(id);
+            if (travel == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ClientId = new SelectList(db.Clients, "Id", "FIO", travel.ClientId);
+            return View(travel);
+        }
+
+        // POST: Travels/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,ClientId,DateCreate,TotalCost")] Travel travel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(travel).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ClientId = new SelectList(db.Clients, "Id", "FIO", travel.ClientId);
+            return View(travel);
+        }
+
+        // GET: Travels/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Travel travel = db.Travels.Find(id);
+            if (travel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(travel);
+        }
+
+        // POST: Travels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Travel travel = db.Travels.Find(id);
+            db.Travels.Remove(travel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
